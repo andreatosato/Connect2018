f9cff8843ef74eb4f4b36deee049559b348433b1
Andrea Tosato <andrea.tosato@hotmail.it>
2018-12-08 16:24:13 +0100

    Complete C# 8 example

--- ConnectWPF2018/MainWindow.xaml.cs
+++ ConnectWPF2018/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+// IAsyncEnumerator --- Fix con il file esterno
+using System.Threading.Tasks;
 
 namespace ConnectWPF2018
 {
@@ -28,6 +22,102 @@ namespace ConnectWPF2018
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void NullReference_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            System.Diagnostics.Debug.WriteLine($"The first letter of {s} is {s[0]}");
+        }
+
+        // <NullableReferenceTypes>true</NullableReferenceTypes>
+        private void NullReferenceNullable_Click(object sender, RoutedEventArgs e)
+        {
+            string? s = null;
+            if (s != null) System.Diagnostics.Debug.WriteLine($"The first letter of {s} is {s[0]}");
+        }
+
+        // <NullableReferenceTypes>true</NullableReferenceTypes>
+        private void NullReferenceOperator_Click(object sender, RoutedEventArgs e)
+        {
+            string? s = null;
+            // null conditional indexing operator
+            System.Diagnostics.Debug.WriteLine($"The first letter of {s} is {s?[0] ?? '?'}");
+        }
+
+
+        private void Range_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(" -- ");
+            foreach (var name in GetNames())
+            {
+                System.Diagnostics.Debug.WriteLine(name);
+            }
+        }
+
+        private void RangeExample1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(" -- ");
+            var names = GetNames().ToArray();
+            Range range = 1..4;
+            foreach (var name in names[range])
+            {
+                System.Diagnostics.Debug.WriteLine(name);
+            }
+        }
+
+        private void RangeExample2_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(" -- ");
+            var names = GetNames().ToArray();
+            // Fino a 1 elemento dalla fine dell'array
+            /*
+             * Range expressions can be open at either or both ends. ..^1 means the same as 0..^1. 
+             * 1.. means the same as 1..^0. 
+             * And .. means the same as 0..^0: beginning to end. 
+             */
+            Range range = 1..^1;
+            foreach (var name in names[range])
+            {
+                System.Diagnostics.Debug.WriteLine(name);
+            }
+        }
+        private void AsyncStream_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await foreach (var name in GetNamesAsync())
+                {
+                    System.Diagnostics.Debug.WriteLine(name);
+                }
+            })
+            .ConfigureAwait(false).GetAwaiter().GetResult();            
+        }
+
+
+        static IEnumerable<string> GetNames()
+        {
+            string[] names =
+            {
+                "Archimedes", "Pythagoras", "Euclid", "Socrates", "Plato"
+            };
+            foreach (var name in names)
+            {
+                yield return name;
+            }
+        }
+
+        static async IAsyncEnumerable<string> GetNamesAsync()
+        {
+            string[] names =
+            {
+                "Archimedes", "Pythagoras", "Euclid", "Socrates", "Plato"
+            };
+            foreach (var name in names)
+            {
+                await Task.Delay(1000);
+                yield return name;
+            }
         }
     }
 }
